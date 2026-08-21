4cb446d416608ef8ab819d9fc3496125199df117
99Liner <denicejaquez0@gmail.com>
2026-08-21 18:34:54 -0400

    next.config.js static for git pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  output: 'export', // Outputs a static HTML/CSS/JS 'out' folder
-  basePath: isProd ? '/my-page' : '', 
+  output: 'export',
+  basePath: isProd ? '/my-page' : '',
   images: {
-    unoptimized: true, // GitHub Pages doesn't support the Next.js image server
+    unoptimized: true,
   },
+
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
