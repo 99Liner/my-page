9be8e58b4bd8c59d3546e961d905816e02f19398
99Liner <denicejaquez0@gmail.com>
2026-08-22 00:14:12 -0400

    .

---
+++ src/app/api/test-db/route.js
@@ -0,0 +1,27 @@
+import clientPromise from '../../../lib/mongodb';
+
+export async function GET() {
+  try {
+    const client = await clientPromise;
+
+    const db = client.db('Portfolio');
+
+    const projects = await db
+      .collection('Projects')
+      .find({})
+      .toArray();
+
+    return Response.json({
+      success: true,
+      projects,
+    });
+  } catch (error) {
+    return Response.json(
+      {
+        success: false,
+        error: error.message,
+      },
+      { status: 500 }
+    );
+  }
+}
